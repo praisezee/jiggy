ddf20d752cc65beaacd1831f63f0564a782f4f9e
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2023-09-27 14:46:25 +0100

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
